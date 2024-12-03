7b9f57f23ab74dec3a2a51e8942ebe368185a127
Mingfun Wong <mingfun.wong.chn@gmail.com>
2024-12-03 20:44:22 +0900

    update

--- start-snapshot.sh
+++ start-snapshot.sh
@@ -2,17 +2,12 @@
 set -e
 
 NODE_DIR=/node/data
+SNAPSHOT_DIR=/node/data/snapshots/
 SNAPSHOT_PATH=/node/data/snapshots/snapshots.bin
 
-rm -rf $NODE_DIR/blocks
-rm -rf $NODE_DIR/protocol_features
-rm -rf $NODE_DIR/snapshots/*
-rm -rf $NODE_DIR/state
-rm -rf $NODE_DIR/config.ini
+rm -rf $NODE_DIR/*
 
-if [ ! -d $NODE_DIR ];then
-    mkdir $NODE_DIR
-fi
+mkdir -p $SNAPSHOT_DIR
 
 if [ ! $SNAPSHOT_URL ]; then  
     SNAPSHOT_URL=$(curl https://api.fibos123.com/last_snapshot)
